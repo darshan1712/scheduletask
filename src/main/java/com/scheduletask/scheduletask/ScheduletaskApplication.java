4ccce738b8117c01876ab68df37d073452bbfede
Darshan Mehta <darshanm@cybage.com>
2022-12-24 20:04:17 +0530

    created scheduler

--- src/main/java/com/scheduletask/scheduletask/ScheduletaskApplication.java
+++ src/main/java/com/scheduletask/scheduletask/ScheduletaskApplication.java
@@ -2,8 +2,10 @@ package com.scheduletask.scheduletask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ScheduletaskApplication {
 
 	public static void main(String[] args) {
